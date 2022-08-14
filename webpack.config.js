e18f37a67945de4639763891be3717e527968bc3
yuuki45 <touya_ayase@yahoo.co.jp>
2022-08-14 15:47:07 +0900

    generate html through pug

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,20 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
@@ -41,7 +55,12 @@ module.exports = {
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ],
